1b21d3d267ba6906dc92c66952647ea33e9c4e01
huynhdq <doquanghuynh2911@gmail.com>
2023-10-02 15:50:34 +0700

    test 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hello");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
